4473bd0f05c65fdbd9030b3d806ec8e737d7ce17
Pratik Savla <pratiksavla07@gmail.com>
2018-11-03 10:54:03 +0530

    Update MyBot-v3.py

--- MyBot-v3.py
+++ MyBot-v3.py
@@ -12,3 +12,30 @@ game.ready("Pratikbot")
 
 ship_states = {}
 i=0
+while True:
+    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
+    game.update_frame()
+    # You extract player metadata and the updated map metadata here for convenience.
+    me = game.me
+
+    '''comes from game, game comes from before the loop, hlt.Game points to networking, which is where you will
+    find the actual Game class (hlt/networking.py). From here, GameMap is imported from hlt/game_map.py.
+
+    open that file to seee all the things we do with game map.'''
+    game_map = game.game_map  # game map data. Recall game is
+
+    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+    #   end of the turn.
+    command_queue = []
+
+    # specify the order we know this all to be
+    direction_order = [Direction.North, Direction.South, Direction.East, Direction.West, Direction.Still]
+
+    position_choices = []
+    for ship in me.get_ships():
+        if ship.id not in ship_states:
+            ship_states[ship.id] = "collecting"
+
+        if len(me.get_ships())/(len(me.get_dropoffs())+1)>5 and me.halite_amount>5000 and i==0 and game_map.calculate_distance(ship.position, me.shipyard.position)>10:
+            command_queue.append(ship.make_dropoff())
+            i=1
